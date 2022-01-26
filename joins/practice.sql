e055666676963e8b81cc2be475170de80cd9ae63
nithishnithish18 <nithishprabakaran09@gmail.com>
2022-01-26 17:16:25 +0530

    left-join day2

--- joins/practice.sql
+++ joins/practice.sql
@@ -1,5 +1,5 @@
 
-#inner join
+#1. inner join
 #joining two tables
 select * from employees;
 select * from departments;
@@ -12,3 +12,32 @@ where emp.department_id in  (1, 2, 3, 4, 5, 6);
 select emp.first_name, dept.department_id,dept.department_name,job.job_title from employees emp
 inner join departments dept on emp.department_id = dept.department_id
 inner join jobs job on emp.job_id = job.job_id;
+use hr_db;
+show tables;
+
+#left join 
+#Joining two tables(tables - locations, countries)
+select * from locations;
+select * from countries;
+SELECT
+	c.country_name,
+	c.country_id,
+	l.country_id,
+	l.street_address,
+	l.city
+FROM
+	countries c 
+left join locations l on c.country_id = l.country_id;
+
+#joining three tables(tables - regions, countries, locations)
+select
+	r.region_id,
+    	c.region_id,
+	c.country_name,
+    	c.country_id,
+    	l.country_id,
+    	l.city
+from
+	regions r
+left join countries c on c.region_id = r.region_id
+left join locations l on l.country_id = c.country_id;
